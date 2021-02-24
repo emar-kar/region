2de7d276f9b1dd22cebb32ccffcf6c0da01ce5b7
Leonid Emar-Kar <eleonid@flightpath3d.com>
2021-02-24 21:07:45 +0300

    v2.5.0

--- v2/region.go
+++ v2/region.go
@@ -1,37 +1,35 @@
-// Package utils implements functions to calculate tiles range according to the given coordinates.
+// Package region implements functions to calculate tiles range according to the given coordinates.
 package region
 
 import (
-	"errors"
 	"math"
-	"strconv"
-	"strings"
 )
 
-// ErrLvlRange raises in case if one of the levels is out of range 0-16.
-var ErrLvlRange = errors.New("levels range is broken")
+// Coordinates represents map coordinates.
+type Coordinates struct {
+	MaxLat float64
+	MaxLon float64
+	MinLat float64
+	MinLon float64
+}
 
-// ErrLvlParse raises in case if the level string is incorrect.
-var ErrLvlParse = errors.New("cannot parse levels")
-
-// Coordinates represents map with coordinates.
-type Coordinates map[string]float64
-
+// Tiles represents data about current tiles map.
 type Tiles struct {
 	Level       int
 	Range       *TilesRange
-	Coordinates Coordinates
+	Coordinates *Coordinates
 }
 
+// TilesRange represents range of the tiles.
 type TilesRange struct {
-	MaxLat int
-	MaxLon int
-	MinLat int
-	MinLon int
+	MaxY int
+	MaxX int
+	MinY int
+	MinX int
 }
 
-// AbsCoordinates returns coordinates of the nearest tile corner.
-func AbsCoordinates(curLvl, accuracy int, fld float64, originalCoordinates Coordinates) Coordinates {
+// CornerCoordinates returns coordinates of the nearest tile corner.
+func CornerCoordinates(curLvl, accuracy int, fld float64, originalCoordinates *Coordinates) *Coordinates {
 	absCoords := Coordinates{}
 
 	if accuracy != -1 {
@@ -41,83 +39,27 @@ func AbsCoordinates(curLvl, accuracy int, fld float64, originalCoordinates Coord
 	rangeX := 360 / ((fld * (math.Pow(2, float64(curLvl)))) * 2)
 	rangeY := 180 / (fld * (math.Pow(2, float64(curLvl))))
 
-	absCoords["minLat"] = math.Floor((originalCoordinates["minLat"]+90)/rangeY)*rangeY - 90
-	absCoords["minLon"] = math.Floor((originalCoordinates["minLon"]+180)/rangeX)*rangeX - 180
-	absCoords["maxLat"] = math.Ceil((originalCoordinates["maxLat"]+90)/rangeY)*rangeY - 90
-	absCoords["maxLon"] = math.Ceil((originalCoordinates["maxLon"]+180)/rangeX)*rangeX - 180
+	absCoords.MaxLat = math.Ceil((originalCoordinates.MaxLat+90)/rangeY)*rangeY - 90
+	absCoords.MaxLon = math.Ceil((originalCoordinates.MaxLon+180)/rangeX)*rangeX - 180
+	absCoords.MinLat = math.Floor((originalCoordinates.MinLat+90)/rangeY)*rangeY - 90
+	absCoords.MinLon = math.Floor((originalCoordinates.MinLon+180)/rangeX)*rangeX - 180
 
-	return absCoords
-}
-
-// GetCoordinates parses coordinates from the given parameters.
-func GetCoordinates(args []string) (*Coordinates, error) {
-	var coordinatesVars = [4]string{"minLat", "minLon", "maxLat", "maxLon"}
-	coords := Coordinates{}
-
-	for ind, el := range coordinatesVars {
-		current, err := strconv.ParseFloat(RemoveCharacters(args[ind], ", "), 64)
-		if err != nil {
-			return nil, err
-		}
-
-		coords[el] = current
-	}
-	return &coords, nil
-}
-
-// GetLvls parses levels from parameters.
-func GetLvls(s string) (int, int, error) {
-	lvlsString := strings.Split(RemoveCharacters(s, " "), "-")
-	if len(lvlsString) != 2 {
-		return 0, 0, ErrLvlParse
-	}
-
-	fromLvl, err := strconv.Atoi(lvlsString[0])
-	if err != nil {
-		return 0, 0, err
-	}
-
-	toLvl, err := strconv.Atoi(lvlsString[1])
-	if err != nil {
-		return 0, 0, err
-	}
-
-	if toLvl > 16 || fromLvl < 0 || toLvl < fromLvl {
-		return 0, 0, ErrLvlRange
-	}
-
-	return fromLvl, toLvl, nil
+	return &absCoords
 }
 
 // GetTiles returns tiles range for given parameters.
 func GetTiles(lvl, accuracy int, fld float64, coords *Coordinates) *Tiles {
-	absCoords := AbsCoordinates(lvl, accuracy, fld, *coords)
+	cornerCoords := CornerCoordinates(lvl, accuracy, fld, coords)
 
 	rangeX := 360 / ((fld * (math.Pow(2, float64(lvl)))) * 2)
 	rangeY := 180 / (fld * (math.Pow(2, float64(lvl))))
 
 	tilesRange := TilesRange{}
 
-	tilesRange.MinLat = int((absCoords["minLat"] + 90) / rangeY)
-	tilesRange.MinLon = int((absCoords["minLon"] + 180) / rangeX)
-	tilesRange.MaxLat = int((absCoords["maxLat"]+90)/rangeY) - 1
-	tilesRange.MaxLon = int((absCoords["maxLon"]+180)/rangeX) - 1
+	tilesRange.MaxY = int((cornerCoords.MaxLat+90)/rangeY) - 1
+	tilesRange.MaxX = int((cornerCoords.MaxLon+180)/rangeX) - 1
+	tilesRange.MinY = int((cornerCoords.MinLat + 90) / rangeY)
+	tilesRange.MinX = int((cornerCoords.MinLon + 180) / rangeX)
 
-	return &Tiles{Level: lvl, Range: &tilesRange, Coordinates: absCoords}
-
-	// fmt.Printf("For level %d\n", lvl)
-	// fmt.Printf("y range = %d - %d, x range = %d - %d\n", minLat, maxLat-1, minLon, maxLon-1)
-	// fmt.Printf("Min coordinates: %v, %v\n", absCoords["minLat"], absCoords["minLon"])
-	// fmt.Printf("Max coordinates: %v, %v\n", absCoords["maxLat"], absCoords["maxLon"])
-}
-
-// RemoveCharacters removes determined characters from the given string.
-func RemoveCharacters(input string, characters string) string {
-	filter := func(r rune) rune {
-		if !strings.ContainsRune(characters, r) {
-			return r
-		}
-		return -1
-	}
-	return strings.Map(filter, input)
+	return &Tiles{Level: lvl, Range: &tilesRange, Coordinates: cornerCoords}
 }
--- v2/region_test.go
+++ v2/region_test.go
@@ -1,41 +1,23 @@
 package region
 
 import (
-	"errors"
 	"fmt"
-	"reflect"
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func ExampleGetLvls() {
-	levels := "7 - 9"
-	fromLvl, toLvl, err := GetLvls(levels)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Printf("from level: %d\nto level: %d\n", fromLvl, toLvl)
-
-	// Output:
-	// from level: 7
-	// to level: 9
-}
-
 func ExampleGetTiles() {
 	fromLvl, toLvl, accuracy := 10, 10, -1
 	fld := float64(5)
-	coords := &Coordinates{"maxLat": 60.120754, "maxLon": 30.699476, "minLat": 59.805323, "minLon": 30.024240}
+	coords := &Coordinates{MaxLat: 60.120754, MaxLon: 30.699476, MinLat: 59.805323, MinLon: 30.024240}
 
 	for lvl := fromLvl; lvl <= toLvl; lvl++ {
 		tiles := GetTiles(lvl, accuracy, fld, coords)
 		fmt.Printf("For level %d\n", tiles.Level)
-		fmt.Printf("y range = %d - %d, x range = %d - %d\n", tiles.Range.MinLat, tiles.Range.MaxLat, tiles.Range.MinLon, tiles.Range.MaxLon)
-		fmt.Printf("Min coordinates: %v, %v\n", tiles.Coordinates["minLat"], tiles.Coordinates["minLon"])
-		fmt.Printf("Max coordinates: %v, %v\n", tiles.Coordinates["maxLat"], tiles.Coordinates["maxLon"])
+		fmt.Printf("y range = %d - %d, x range = %d - %d\n", tiles.Range.MinY, tiles.Range.MaxY, tiles.Range.MinX, tiles.Range.MaxX)
+		fmt.Printf("Min coordinates: %v, %v\n", tiles.Coordinates.MinLat, tiles.Coordinates.MinLon)
+		fmt.Printf("Max coordinates: %v, %v\n", tiles.Coordinates.MaxLat, tiles.Coordinates.MaxLon)
 	}
 
 	// Output:
@@ -45,140 +27,54 @@ func ExampleGetTiles() {
 	// Max coordinates: 60.15234375, 30.7265625
 }
 
-func TestAbsCoordinates(t *testing.T) {
+func TestCornerCoordinates(t *testing.T) {
 	tests := []struct {
 		name       string
 		curLvl     int
 		accuracy   int
 		fld        float64
-		origCoords Coordinates
-		want       Coordinates
+		origCoords *Coordinates
+		want       *Coordinates
 	}{
 		{
 			name:       "correct curLvl=10 accuracy=-1 fld=5",
 			curLvl:     10,
 			accuracy:   -1,
 			fld:        5,
-			origCoords: Coordinates{"maxLat": 60.120754, "maxLon": 30.699476, "minLat": 59.805323, "minLon": 30.024240},
-			want:       Coordinates{"maxLat": 60.15234375, "maxLon": 30.7265625, "minLat": 59.80078125, "minLon": 30.0234375},
+			origCoords: &Coordinates{MaxLat: 60.120754, MaxLon: 30.699476, MinLat: 59.805323, MinLon: 30.024240},
+			want:       &Coordinates{MaxLat: 60.15234375, MaxLon: 30.7265625, MinLat: 59.80078125, MinLon: 30.0234375},
 		},
 		{
 			name:       "correct curLvl=10 accuracy=-1 fld=2",
 			curLvl:     10,
 			accuracy:   -1,
 			fld:        2,
-			origCoords: Coordinates{"maxLat": 60.120754, "maxLon": 30.699476, "minLat": 59.805323, "minLon": 30.024240},
-			want:       Coordinates{"maxLat": 60.205078125, "maxLon": 30.76171875, "minLat": 59.765625, "minLon": 29.970703125},
+			origCoords: &Coordinates{MaxLat: 60.120754, MaxLon: 30.699476, MinLat: 59.805323, MinLon: 30.024240},
+			want:       &Coordinates{MaxLat: 60.205078125, MaxLon: 30.76171875, MinLat: 59.765625, MinLon: 29.970703125},
 		},
 		{
 			name:       "correct curLvl=10 accuracy=6 fld=5",
 			curLvl:     10,
 			accuracy:   6,
 			fld:        5,
-			origCoords: Coordinates{"maxLat": 60.120754, "maxLon": 30.699476, "minLat": 59.805323, "minLon": 30.024240},
-			want:       Coordinates{"maxLat": 60.1875, "maxLon": 30.9375, "minLat": 59.625, "minLon": 29.8125},
+			origCoords: &Coordinates{MaxLat: 60.120754, MaxLon: 30.699476, MinLat: 59.805323, MinLon: 30.024240},
+			want:       &Coordinates{MaxLat: 60.1875, MaxLon: 30.9375, MinLat: 59.625, MinLon: 29.8125},
 		},
 		{
 			name:       "correct curLvl=10 accuracy=6 fld=2",
 			curLvl:     10,
 			accuracy:   6,
 			fld:        2,
-			origCoords: Coordinates{"maxLat": 60.120754, "maxLon": 30.699476, "minLat": 59.805323, "minLon": 30.024240},
-			want:       Coordinates{"maxLat": 60.46875, "maxLon": 30.9375, "minLat": 59.0625, "minLon": 29.53125},
+			origCoords: &Coordinates{MaxLat: 60.120754, MaxLon: 30.699476, MinLat: 59.805323, MinLon: 30.024240},
+			want:       &Coordinates{MaxLat: 60.46875, MaxLon: 30.9375, MinLat: 59.0625, MinLon: 29.53125},
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			absCoords := AbsCoordinates(tc.curLvl, tc.accuracy, tc.fld, tc.origCoords)
-			assert.Equal(t, tc.want, absCoords)
+			cornerCoords := CornerCoordinates(tc.curLvl, tc.accuracy, tc.fld, tc.origCoords)
+			assert.Equal(t, tc.want, cornerCoords)
 		})
 	}
 
-}
-
-func TestGetLvls(t *testing.T) {
-	tests := []struct {
-		name    string
-		input   string
-		fromLvl int
-		toLvl   int
-		err     error
-	}{
-		{name: "case 7-9", input: "7-9", fromLvl: 7, toLvl: 9, err: nil},
-		{name: "case 0-11", input: "0-11", fromLvl: 0, toLvl: 11, err: nil},
-		{name: "case 0 - 11", input: "0-11", fromLvl: 0, toLvl: 11, err: nil},
-		{name: "err with levels parsing", input: "712", fromLvl: 0, toLvl: 0, err: ErrLvlParse},
-		{name: "err with fromLvl", input: "Z-9", fromLvl: 0, toLvl: 0, err: strconv.ErrSyntax},
-		{name: "err with toLvl", input: "7-Z", fromLvl: 0, toLvl: 0, err: strconv.ErrSyntax},
-		{name: "err with range", input: "7-20", fromLvl: 0, toLvl: 0, err: ErrLvlRange},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			fromLvl, toLvl, err := GetLvls(tc.input)
-			if fromLvl != tc.fromLvl || toLvl != tc.toLvl || !errors.Is(err, tc.err) {
-				t.Errorf("%s: expected: %d %d, %v; got: %v %v, %v",
-					tc.name, tc.fromLvl, tc.toLvl, tc.err, fromLvl, toLvl, err)
-			}
-		})
-	}
-}
-
-func TestGetCoordinates(t *testing.T) {
-	tests := []struct {
-		name   string
-		input  []string
-		coords *Coordinates
-		err    error
-	}{
-		{
-			name:   "correct",
-			input:  []string{"59.805323, ", "30.024240", "60.120754, ", "30.699476"},
-			coords: &Coordinates{"minLat": 59.805323, "minLon": 30.024240, "maxLat": 60.120754, "maxLon": 30.699476},
-			err:    nil,
-		},
-		{
-			name:   "error",
-			input:  []string{"59.805323, ", "30.0Z4240", "60.120754, ", "30.699476"},
-			coords: nil,
-			err:    strconv.ErrSyntax,
-		},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			coords, err := GetCoordinates(tc.input)
-			if errors.Is(err, tc.err) {
-				assert.Equal(t, tc.coords, coords, "coordinates should be equal")
-			} else {
-				t.Errorf("%s: expected: %#v, %v; got: %v, %v", tc.name, tc.coords, tc.err, coords, err)
-			}
-		})
-	}
-}
-
-func TestRemoveCharacters(t *testing.T) {
-	tests := []struct {
-		name   string
-		input  string
-		remove string
-		want   string
-	}{
-		{name: "letter", input: "test string", remove: "t", want: "es sring"},
-		{name: "letters", input: "test string", remove: "ts", want: "e ring"},
-		{name: "whitespace", input: "test string", remove: " ", want: "teststring"},
-		{name: "hyphen", input: "test-string", remove: "-", want: "teststring"},
-		{name: "punctuation mark", input: "test, string", remove: ",", want: "test string"},
-		{name: "punctuation marks", input: "test, string.", remove: ",.", want: "test string"},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			got := RemoveCharacters(tc.input, tc.remove)
-			if !reflect.DeepEqual(tc.want, got) {
-				t.Errorf("%s: expected: %s; got: %v", tc.name, tc.want, got)
-			}
-		})
-	}
 }
